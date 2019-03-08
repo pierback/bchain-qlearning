c97c4f2ed4b1cb3aace7133ea023a83ba88179b6
Fabian <fabian.pieringer@student.uni-augsburg.de>
2019-03-08 21:31:33 +0100

    udp listener for server ip

---
+++ pkg/utils/listener.go
@@ -0,0 +1,65 @@
+package utils
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+const (
+	defaultMulticastAddress = "239.0.0.0:9999"
+	maxDatagramSize         = 8192
+)
+
+// func GetServerIP(done chan string) string {
+func GetServerIP() string {
+	ip := Listen(defaultMulticastAddress)
+	fmt.Println("ip: ", ip)
+	if ip != "" {
+		fmt.Println("ip ip ip not null", ip)
+		// done <- ip
+		return ip
+	}
+
+	ipStr := fmt.Sprintf("%s", GetLocalIP())
+	return ipStr
+}
+
+// Listen binds to the UDP address and port given and writes packets received
+// from that address to a buffer which is passed to a hander
+func Listen(address string) string {
+	addr, err := net.ResolveUDPAddr("udp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Open up a connection
+	conn, err := net.ListenMulticastUDP("udp", nil, addr)
+	if err != nil {
+		log.Println(err)
+	}
+
+	conn.SetReadBuffer(maxDatagramSize)
+
+	ch := make(chan string, 1)
+	go checkUDPCast(ch, conn)
+
+	select {
+	case ip, _ := <-ch:
+		return ip
+	case <-time.After(1 * time.Second):
+		fmt.Println("Timed out: no udp-cast available")
+		return ""
+	}
+}
+
+func checkUDPCast(ch chan string, conn *net.UDPConn) {
+	buffer := make([]byte, maxDatagramSize)
+	numBytes, _, err := conn.ReadFromUDP(buffer)
+	if err != nil {
+		log.Println("ReadFromUDP failed:", err)
+	}
+	ip := string(buffer[:numBytes])
+	ch <- ip
+}
