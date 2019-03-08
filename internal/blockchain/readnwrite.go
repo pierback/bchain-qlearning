cea9e56ab760e5c768864edc927f272d41b971e6
Fabian <fabian.pieringer@student.uni-augsburg.de>
2019-03-08 21:32:20 +0100

    minor changes

---
+++ internal/blockchain/readnwrite.go
@@ -0,0 +1,51 @@
+package blockchain
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	bl "github.com/pierback/bchain-qlearning/internal/contracts/BeverageList"
+	ut "github.com/pierback/bchain-qlearning/pkg/utils"
+)
+
+func TestBl() {
+	client := ut.GetClientConnection()
+	contractAddress := ut.GetLatestContractAddress()
+
+	fmt.Printf("wait\n\n")
+
+	instance, err1 := bl.NewBeveragelist(contractAddress, client)
+	ut.PrintError(err1)
+
+	setDrinkData(instance)
+}
+
+func setDrinkData(instance *bl.Beveragelist) {
+	auth, _ := bind.NewTransactor(strings.NewReader(key), "password")
+	drink := [32]byte{}
+	weekday := [32]byte{}
+	ti := [32]byte{}
+	copy(drink[:], []byte("mate"))
+	copy(weekday[:], []byte(time.Now().Weekday().String()))
+	copy(ti[:], []byte(time.Now().String()))
+
+	_, err := instance.SetDrinkData(auth, ti, drink, weekday)
+
+	ut.PrintError(err)
+}
+
+func getFromTime(instance *bl.Beveragelist, t [32]byte) {
+	drink, weekday, _ := instance.GetDrinkData(nil, t)
+	fmt.Println("getFromTime drink", string(drink[:]))     // foo
+	fmt.Println("getFromTime weekday", string(weekday[:])) // foo
+}
+
+func getLastDrink(instance *bl.Beveragelist) {
+	addr, drink, weekday, _ := instance.LastDrink(nil)
+
+	fmt.Println("getDrinkData id", addr.Hex())              // foo
+	fmt.Println("getDrinkData drink", string(drink[:]))     // foo
+	fmt.Println("getDrinkData weekday", string(weekday[:])) // foo
+}
--- internal/blockchain/watcher.go
+++ internal/blockchain/watcher.go
@@ -15,18 +15,15 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 
 	bl "github.com/pierback/bchain-qlearning/internal/contracts/BeverageList"
+	ut "github.com/pierback/bchain-qlearning/pkg/utils"
 )
 
 // const key = `{"address":"02e9f84165314bb8c255d8d3303b563b7375eb61","crypto":{"cipher":"aes-128-ctr","ciphertext":"f4952ba9725d5ae83f6e2c47714e4a1ed533a60a4ea97d635fd674e84b419f8a","cipherparams":{"iv":"98de0f7f0469a83ee3f543b232a9ec67"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"7629ecaf07277427a80e8e3138d9fc4058ca63b004f7ba3a2b253f22db55bced"},"mac":"b10cc5c56aa64f2246a5786ea3247ab6a133961b3b62985bc999dcc7cb51ade7"},"id":"3dfb452c-af90-41aa-aa8f-6da654b818e9","version":3}`
 const key = `{"address":"e8816898d851d5b61b7f950627d04d794c07ca37","crypto":{"cipher":"aes-128-ctr","ciphertext":"1ff4add6955cba7ddaf29f66d7d21c5e1d714ef6191fbc651ae60f2ea3c95e8f","cipherparams":{"iv":"3ff869fbdbe1a523cdb327780365976e"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"7372dbae5fb318f8684902e099c311d4188721d677974d729711762c7ef6030c"},"mac":"485fa5dc701067782baa1589716a53110c7f917eb259e35ebca7265bbb7150b1"},"id":"89edb004-5b00-4607-a3af-a0d9ab9b1c34","version":3}`
 
 func Watch() {
-	client, err := ethclient.Dial("ws://0.0.0.0:8546")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	contractAddress := getLatestContractAddress()
+	client := ut.GetClientConnection()
+	contractAddress := ut.GetLatestContractAddress()
 
 	query := ethereum.FilterQuery{
 		Addresses: []common.Address{contractAddress},
@@ -36,10 +33,7 @@ func Watch() {
 	ctx := context.Background()
 
 	sub, err := client.SubscribeFilterLogs(ctx, query, ch)
-
-	if err != nil {
-		log.Println(err)
-	}
+	ut.PrintError(err)
 
 	fmt.Println("Watch")
 
@@ -68,9 +62,7 @@ func readEvent(eventLog types.Log, client *ethclient.Client) {
 	}
 
 	err1 := contractAbi.Unpack(&event, "NewDrink", eventLog.Data)
-	if err1 != nil {
-		log.Fatal(err1)
-	}
+	ut.PrintError(err1)
 
 	fmt.Println(" ")
 	fmt.Println("New Event:")
@@ -84,9 +76,7 @@ func readEvent(eventLog types.Log, client *ethclient.Client) {
 
 func getBlockTransactions(eventLog types.Log, client *ethclient.Client) {
 	block, err := client.BlockByNumber(context.Background(), big.NewInt(int64(eventLog.BlockNumber)))
-	if err != nil {
-		log.Fatal(err)
-	}
+	ut.PrintError(err)
 
 	for _, tx := range block.Transactions() {
 		fmt.Println("Recipient", tx.To().Hex())
